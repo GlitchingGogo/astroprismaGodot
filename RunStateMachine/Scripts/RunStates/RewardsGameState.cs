f5a5da4b1c1be50fa111d3144660cfe98a5e1c40
AshdownGD <33364046+AshdownGD@users.noreply.github.com>
2025-10-17 23:51:21 -0700

    updating reward game state to have a leave button

--- RunStateMachine/Scripts/RunStates/RewardsGameState.cs
+++ RunStateMachine/Scripts/RunStates/RewardsGameState.cs
@@ -3,9 +3,10 @@ using System;
 
 public partial class RewardsGameState : GameState
 {
+    [Export] Button leaveButton;
     public override void EnterState(object parameter = null)
     {
-        //TODO
+        leaveButton.Pressed += () => RunStateMachine.GoToState(RunStateType.Map);
     }
 
     public override void ExitState()
