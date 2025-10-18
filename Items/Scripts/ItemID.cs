6098969593b798a2ac2882c34b55b42b315e0c50
B Wong-Dearing <gogo@feejai.ca>
2025-10-18 19:17:39 -0400

    ItemID
    Added all item IDs for base + expansions

--- Items/Scripts/ItemID.cs
+++ Items/Scripts/ItemID.cs
@@ -2,6 +2,75 @@ public enum ItemID
 {
     None = -1,
 
-    
+    // Currencies
+    Serum,
+    Scraps,
+    VYen,
+
+    // Equipment
+    HealthPack,
+    EnergyCell,
+    StardustSoda,
+    DataShard,
+    BurnPatch,
+    ShieldStim,
+    PowerGen,
+    AntiVirus,
+    AdrenaShot,
+    FragGrenade,
+    EMPGrenade,
+    HeatBomb,
+    TaserShell,
+    FlashRound,
+    StratogenHormones,
+    VesteroneStimulant,
+    TestogreHormones,
+    ElectroxynCapsule,
+    TricillinTablet,
+    SupranovaPhysic,
+    NetLink,
+    ZeroGLiquor,
+
+    // Cloud IX Casino Equipment
+    CloudCigarette,
+    Glitter,
+    RocabellaCrystal,
+    Ambrossia,
+
+    // Quest Items
+    WarpDrive,
+    TradingLicense,
+    Tesseract,
+    ContrabandPackage,
+    NGHTMRKey,
+    TerraformSeeds,
+
+    // Armor
+    ReinforcedJacket,
+    BulletproofVest,
+    NightrunnerClothing,
+    CombatExoskeleton,
+    EagleshotArmor,
+    AthenaXArmor,
+    OrionIIArmor,
+    MercurianArmor,
+    HadesIVArmor,
+    ColossusSuit,
+
+    // Cloud IX Casino Armor
+    KinoJacket,
+    MeteorCoat,
+    MonarchDress,
+    VizierFutureskin,
+
+    // Cloud IX Casino Accessories
+    KinoSneakers,
+    KinoChain,
+    MeteorBoots,
+    MeteorWatch,
+    MonarchHeels,
+    MonarchNecklace,
+    VizierPlatforms,
+    VizierVision,
     Count,
 }
